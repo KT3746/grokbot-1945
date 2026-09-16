2566b6df542db6bb5f7e00fcc2a24c554237b988
KT3746 <tks.front463@passmail.com>
2026-09-16 00:54:01 -0300

    chore: VERSION 1.9.2

--- js/version.js
+++ js/version.js
@@ -1,5 +1,5 @@
 /** Versão única: título, rodapé e todos os ?v= do index.html devem bater com isto. */
-export const VERSION = "1.9.1";
+export const VERSION = "1.9.2";
 export const STORAGE_HIGH = "ceu-de-aco-recorde";
 export const STORAGE_MUTE = "ceu-de-aco-mudo";
 export const STORAGE_HOWTO = "ceu-de-aco-howto";
